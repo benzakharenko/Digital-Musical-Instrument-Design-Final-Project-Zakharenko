6be4cc231c28821321c8cb9a043b9d4138881674
benzakharenko <121130145+benzakharenko@users.noreply.github.com>
2024-04-23 13:15:03 -0400

    Added Gyroscope Control—Experiment 1 with Audio Reactive Haptics

--- SimpleMidiHaptics_Wireless_AUDIOIN/SimpleMidiHaptics_Wireless_AUDIOIN.ino
+++ SimpleMidiHaptics_Wireless_AUDIOIN/SimpleMidiHaptics_Wireless_AUDIOIN.ino
@@ -1,6 +1,5 @@
 #include <Wire.h>
 #include "Adafruit_DRV2605.h"
-#include <Adafruit_LSM6DSOX.h>
 #include <Arduino.h>
 #include <BLEMidi.h>
 
@@ -11,21 +10,6 @@ const uint8_t midi_channel = 0;
 const uint8_t controller_number = 0;
 const float c = 0.2;  // low pass filter coefficient (0 < c <= 1)
 
-float xDisplacement = 0;
-float yDisplacement = 0;
-float zDisplacement = 0;
-
-//defining things from the gyroscope:
-typedef enum {
-  I2C_PROTOCOL,
-  SPI_PROTOCOL,
-  SPI_CSONLY_PROTOCOL
-} SerialProtocol;
-
-Adafruit_LSM6DSOX sox;
-SerialProtocol mode = I2C_PROTOCOL;
-
-
 //initializing functions to be defined later
 void onNoteOn();
 void onNoteOff();
@@ -33,7 +17,6 @@ void onControlChange();
 void connected() {
   Serial.println("Connected");
 }
-
 
 
 //calling the DRV265 class
@@ -46,7 +29,7 @@ void setup() {
   // initialize the vibration motor
   drv.begin();
   drv.selectLibrary(1);
-  drv.setMode(DRV2605_MODE_INTTRIG);
+  drv.setMode(DRV2605_MODE_AUDIOVIBE);
 
   // Initialize the BLE MIDI server
   BLEMidiServer.begin("MIDI Glove");
@@ -54,8 +37,6 @@ void setup() {
   BLEMidiServer.setOnDisconnectCallback([]() {  // To show how to make a callback with a lambda function
     Serial.println("Disconnected");
   });
-
-sox.begin_I2C();
 }
 
 void loop() {
@@ -63,7 +44,7 @@ void loop() {
   BLEMidiServer.setNoteOnCallback(onNoteOn);
   BLEMidiServer.setNoteOffCallback(onNoteOff);
   BLEMidiServer.setControlChangeCallback(onControlChange);
-  // for flex sensor ---
+  // for flex sensor
   static float y = 0;
   static uint8_t old_y = 0;
 
@@ -75,62 +56,19 @@ void loop() {
     BLEMidiServer.controlChange(midi_channel, controller_number, y);
     old_y = (uint8_t)y;
   }
-
-  // gyroscopic sensors
-  sensors_event_t accel, gyro, temp;
-  sox.getEvent(&accel, &gyro, &temp);
-  printAngularVelocity(gyro);
 }
 
 // defining the reading value functions
 void onNoteOn(uint8_t channel, uint8_t note, uint8_t velocity, uint16_t timestamp) {
   Serial.printf("Received note on : channel %d, note %d, velocity %d (timestamp %dms)\n", channel, note, velocity, timestamp);
-  drv.setWaveform(0, 47);
-  drv.go();
+  tone(9, 100);
 }
 
 void onNoteOff(uint8_t channel, uint8_t note, uint8_t velocity, uint16_t timestamp) {
   Serial.printf("Received note off : channel %d, note %d, velocity %d (timestamp %dms)\n", channel, note, velocity, timestamp);
+  noTone(9);
 }
 
 void onControlChange(uint8_t channel, uint8_t controller, uint8_t value, uint16_t timestamp) {
   Serial.printf("Received control change : channel %d, controller %d, value %d (timestamp %dms)\n", channel, controller, value, timestamp);
-}
-
-void printAngularVelocity(sensors_event_t gyro) {
-  // Angular velocity is measured in radians/s
-  float x = gyro.gyro.x;
-  float y = gyro.gyro.y;
-  float z = gyro.gyro.z;
-
-  //integration calculation for x
-  float xInMs = x * 0.001; //convert to rad/ms
-  float xArea = xInMs * 200; //integration calculation as rectangle estimation
-  xDisplacement = xDisplacement + xArea;
-  //integration calculation for y
-  float yInMs = y * 0.001; //convert to rad/ms
-  float yArea = yInMs * 200; //integration calculation as rectangle estimation
-  yDisplacement = yDisplacement + yArea;
-  //integration calculation for z
- // float zInMs = z * 0.001; //convert to rad/ms
- // float zArea = zInMs * 200; //integration calculation as rectangle estimation    
- // zDisplacement = zDisplacement + zArea;
-
-  delay(50);
-  Serial.print("x/ ");
-  Serial.println(xDisplacement);
-
-  if (x > 0.1 || x < -0.1)
-  { // -30 to 30
-    int xDeltaToMIDI = map(xDisplacement, -15, 15, 0, 127);
-    BLEMidiServer.controlChange(0, 20, xDeltaToMIDI);
-
-  }
-  if (y > 0.1 || y < -0.1)
-  { //scale -25 to 25
-    int yDeltaToMIDI = map(yDisplacement, -25, 25, 0, 127);
-    //BLEMidiServer.controlChange(0, 21, yDeltaToMIDI);
-  }
-\
-
 }
